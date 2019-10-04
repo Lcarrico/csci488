3bf1aa72d44df140d844236b009a208e63e57851
Leo Carrico <lcarrico827@gmail.com>
2019-10-04 06:10:43 -0500

    looping through cols fix 2

--- lesson7/echo.php
+++ lesson7/echo.php
@@ -1,9 +1,4 @@
-<?
-// $_GET and $_POST are built-in PHP SuperGlobals that handle those kind of form submissions, respectively.
-//The line below merges both into one array so that this script will handle both GET and POST form submissions.
-
-$submitted_form_data = array_merge($_GET,$_POST);
-?>
+<? $submitted_form_data = array_merge($_GET,$_POST); ?>
 
 <!DOCTYPE html>
 <html>
@@ -11,25 +6,19 @@ $submitted_form_data = array_merge($_GET,$_POST);
   <title>Echo of HTML Form Submission</title>
 </head>
 <body>
-
-
 <table width="" cellspacing="0" cellpadding="3">
     <tr>
-
-
       <? $cols = array("name", "email", "age","mood","bio","youare","gender","graduation","fav_building","visited_buildings");
-      foreach ($cols as $title){
-        ?>
-      <th><?=$title?></th>
-
+      foreach ($cols as $title){ ?>
+        <th><?=$title?></th>
       <? } ?>
     </tr>
     <tr>
-    <?foreach ($cols as $key) { ?>
-      <td><?=$submitted_form_data[$key]?></td>
-    <? } ?>
+      <?foreach ($cols as $key) {
+        $value = $submitted_form_data[$key]; ?>
+        <td><?=(is_array($value) ? implode($value, "<br>") : $value)?></td>
+      <? } ?>
     </tr>
-
 </table>
 
 </body>
